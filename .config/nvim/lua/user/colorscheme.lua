6503abc4caed6d12cee87fd8495ac03fefdd6547
Anil Gurses <anilgurses98@gmail.com>
2023-04-26 13:17:40 -0400

    Add new langs to treesitter

--- .config/nvim/lua/user/colorscheme.lua
+++ .config/nvim/lua/user/colorscheme.lua
@@ -6,17 +6,17 @@
 -- }
 
 local M = {
-  "sainnhe/gruvbox-material",
-  commit = "3fff63b0d6a425ad1076a260cd4f6da61d1632b1",
+  "ellisonleao/gruvbox.nvim",
+  commit = "df149bccb19a02c5c2b9fa6ec0716f0c0487feb0",
   lazy = false,    -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
 }
 
-vim.g["gruvbox_material_background"] = "medium"
-vim.g["gruvbox_material_better_performance"] = 1
-vim.g["gruvbox_material_sign_column_background"] = "grey"
+-- vim.g["gruvbox_material_background"] = "medium"
+-- vim.g["gruvbox_material_better_performance"] = 1
+-- vim.g["gruvbox_material_sign_column_background"] = "grey"
 
-M.name = "gruvbox-material"
+M.name = "gruvbox"
 function M.config()
   local status_ok, _ = pcall(vim.cmd.colorscheme, M.name)
   if not status_ok then
