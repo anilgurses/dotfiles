99a91afd7de3df502a1400ce8b2b5abb2c68ce5d
Anil Gurses <anilgurses98@gmail.com>
2023-04-27 17:22:33 -0400

    Add LSPs

--- .config/nvim/lua/utils/init.lua
+++ .config/nvim/lua/utils/init.lua
@@ -1,14 +1,21 @@
 local M = {}
 
+-- List https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 M.servers = {
-  "lua_ls",
-  "cssls",
-  "html",
-  "tsserver",
-  "pyright",
-  "bashls",
-  "jsonls",
-  "yamlls",
+    "lua_ls",
+    "cssls",
+    "html",
+    "tsserver",
+    "pyright",
+    "bashls",
+    "jsonls",
+    "yamlls",
+    "clangd",
+    "cmake",
+    "dockerls",
+    "docker_compose_language_service",
+    "opencl_ls",
+    "tailwindcss"
 }
 
 return M
